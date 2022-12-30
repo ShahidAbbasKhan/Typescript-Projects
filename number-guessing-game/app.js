186828f66ef702da7a89df31b51a4ac66a8c52f9
ShahidAbbasKhan <developershahid297@gmail.com>
2022-12-30 22:28:06 +0500

    adding peojects

---
+++ number-guessing-game/app.js
@@ -0,0 +1,21 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+const randomNumber = Math.floor(Math.random() * 10);
+const responce = await inquirer.prompt({
+    type: "number",
+    name: "Guess_Value",
+    message: chalk.green("Enter one value between 1 to 10 that can guess"),
+});
+const { Guess_Value } = responce;
+if (Guess_Value) {
+    if (Guess_Value === randomNumber) {
+        console.log(chalk.greenBright(`Wow! Your Guess won. You Guess was 
+        ${responce.Guess_Value} that is correct ${randomNumber}`));
+    }
+    else {
+        console.log(chalk.bgBlue("Please Try Again, Your Guess is not corret"));
+    }
+}
+else {
+    console.log(chalk.blue("Enter Value for Guess"));
+}
